6c9d32dac1cfeacf6b3983cc0e345778c389d313
Thomas Lacroix <t.f.m.lacroix@gmail.com>
2019-02-05 19:54:49 +0100

    :tada: added hold function - scene moving - more categories

--- src/index.js
+++ src/index.js
@@ -6,33 +6,135 @@ import * as THREE from 'three'
 
 
 import CameraControls from 'camera-controls';
+
+import Timer from 'tiny-timer'
+
+ 
+let timer = new Timer()
+
+
 CameraControls.install( { THREE: THREE } );
 
+const ctn_home = document.querySelector('.ctn-home')
 const home_btn = document.querySelector('.btn-home')
 const inner_text = document.querySelector('.txt-slider-ctn')
+const cursor_hold = document.querySelector('.cursor-hold')
+const sml_bar = document.querySelector('.sml-bar')
+const numerotation = document.querySelectorAll('.nb-num')
+const numerotation_bar = document.querySelectorAll('.num_bar')
+numerotation[0].style.color = '#000000'
+const createFps = require('fps-indicator')
+let fps = createFps()
 
-const createFps = require('fps-indicator');
-let fps = createFps();
+/**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
 
+window.addEventListener('mousemove', (_event) =>
+{
+    cursor.x = _event.clientX / sizes.width 
+    cursor.y = _event.clientY / sizes.height 
+})
 
 /**
  * Btn home
  */
 let x = 0
 let y = 0
+let hold = false 
+let timing = 0
+let show_cursor = false
 home_btn.addEventListener('click', () =>
 { 
-    x += 10
-    y += 400
-    if(x == 30){
-        x = 0
-        y = 0
+    if(!hold){
+        x += 10
+        y += 400
+
+        if(x == 50){
+            x = 0
+            y = 0
+        }
+        if(x == 0){
+            resetColor(numerotation, '#aaaaaa')
+            numerotation[0].style.color = '#000000'
+        }
+        if(x == 10){
+            resetColor(numerotation, '#aaaaaa')
+            numerotation[1].style.color = '#000000'
+            numerotation_bar[0].style.transformOrigin = 'left'
+            numerotation_bar[0].style.transform = 'scaleX(1)'
+            setTimeout(() => {
+                numerotation_bar[0].style.transformOrigin = 'right'
+                numerotation_bar[0].style.transform = 'scaleX(0)'
+            }, 500)
+        }
+        if(x == 20){
+            resetColor(numerotation, '#aaaaaa')
+            numerotation[2].style.color = '#000000'
+            numerotation_bar[1].style.transformOrigin = 'left'
+            numerotation_bar[1].style.transform = 'scaleX(1)'
+            setTimeout(() => {
+                numerotation_bar[1].style.transformOrigin = 'right'
+                numerotation_bar[1].style.transform = 'scaleX(0)'
+            }, 500)
+        }
+        if(x == 30){
+            resetColor(numerotation, '#aaaaaa')
+            numerotation[3].style.color = '#000000'
+            numerotation_bar[2].style.transformOrigin = 'left'
+            numerotation_bar[2].style.transform = 'scaleX(1)'
+            setTimeout(() => {
+                numerotation_bar[2].style.transformOrigin = 'right'
+                numerotation_bar[2].style.transform = 'scaleX(0)'
+            }, 500)
+        }
+        if(x == 40){
+            resetColor(numerotation, '#aaaaaa')
+            numerotation[4].style.color = '#000000'
+            numerotation_bar[3].style.transformOrigin = 'left'
+            numerotation_bar[3].style.transform = 'scaleX(1)'
+            setTimeout(() => {
+                numerotation_bar[3].style.transformOrigin = 'right'
+                numerotation_bar[3].style.transform = 'scaleX(0)'
+            }, 500)
+        }
+    
+        cameraControls.moveTo(x,0,0,true )
+        inner_text.style.transform = `translateX(${-y}px)`
     }
 
-    cameraControls.moveTo(x,0,0,true )
-    inner_text.style.transform = `translateX(${-y}px)`
-
 })
+home_btn.addEventListener('mousedown', () =>
+{ 
+    setTimeout(() => {
+        hold = true 
+    }, 100);
+    
+})
+
+home_btn.addEventListener('mouseover', () =>
+{ 
+   show_cursor = true 
+})
+home_btn.addEventListener('mouseout', () =>
+{ 
+   show_cursor = false 
+})
+home_btn.addEventListener('mouseup', () =>
+{ 
+    setTimeout(() => {
+        hold = false 
+    }, 100);
+    timing = 0
+})
+function resetColor(el, color){
+    for (let index = 0; index < el.length; index++) {
+        el[index].style.color = color
+    }
+}
 /**
  * Scene
  */
@@ -103,6 +205,35 @@ cone.mesh.position.y = 0
 cone.mesh.position.z = -2.5
 cone.mesh.rotation.x = 0.5
 scene.add(cone.mesh)
+let octa = {}
+octa.geometry = new THREE.OctahedronBufferGeometry( 0.5, 0 );
+octa.material = new THREE.MeshStandardMaterial({
+    color: 0x3f42fa, 
+    flatShading: true,
+    metalness: 0.5,
+    roughness: 1,
+})
+octa.mesh = new THREE.Mesh(octa.geometry, octa.material)
+octa.mesh.position.x = 29
+octa.mesh.position.y = 0
+octa.mesh.position.z = -2.5
+octa.mesh.rotation.x = 0.5
+scene.add(octa.mesh)
+
+let tetra = {}
+tetra.geometry = new THREE.TetrahedronBufferGeometry( 0.5, 0 );
+tetra.material = new THREE.MeshStandardMaterial({
+    color: 0x8842fa, 
+    flatShading: true,
+    metalness: 0.5,
+    roughness: 1
+})
+tetra.mesh = new THREE.Mesh(tetra.geometry, tetra.material)
+tetra.mesh.position.x = 39
+tetra.mesh.position.y = 0
+tetra.mesh.position.z = -2.5
+tetra.mesh.rotation.x = 0.5
+scene.add(tetra.mesh)
 
 
 let cube = {}
@@ -119,6 +250,14 @@ cube.mesh.position.y = 0
 cube.mesh.position.z = -2.5
 cube.mesh.rotation.x = -0.5
 scene.add(cube.mesh)
+
+let cube2 = {}
+cube2.mesh = new THREE.Mesh(cube.geometry, cube.material)
+cube2.mesh.position.x = 0
+cube2.mesh.position.y = 40
+cube2.mesh.position.z = 25
+cube2.mesh.rotation.x = 0
+scene.add(cube2.mesh)
 
 /**
  * Camera
@@ -162,9 +301,45 @@ const loop = () =>
     cone.mesh.position.y = -Math.sin(nb)/4
     cube.mesh.rotation.y -= 0.001
     cube.mesh.position.y = Math.sin(nb)/4
-    // Renderer
+    cube2.mesh.rotation.x += 0.001
+    cube2.mesh.rotation.y += 0.001
+    cube2.mesh.position.y = 40 + Math.sin(nb)/4
+    // Renderer40 + 
     renderer.render(scene, camera)
+    if(hold){
+        timing += 1
+        
+        if(timing > 30){
+            ctn_home.style.opacity = 0 
+            setTimeout(() => {
+                cameraControls.moveTo(x,0,30,true) 
+                
+            }, 500);
+            setTimeout(() => {
+                cameraControls.moveTo(x+1,40,30,true) 
+
+            }, 1000);
+            setTimeout(() => {
+                ctn_home.style.opacity = 1 
+            }, 2000);
+        }
+        else{
+            sml_bar.style.transform = `scaleX(${timing/30})`
+        }
+            
+    }
+    else 
+    sml_bar.style.transform = `scaleX(0)`
+    if(show_cursor){
+        cursor_hold.style.opacity = '1'
+        cursor_hold.style.left = `${(cursor.x*100)+1.5}%`
+        cursor_hold.style.top = `${(cursor.y*100)+2}%`
+    }
+    else 
+        cursor_hold.style.opacity = '0'
+
     
+
 }
 loop()
 
